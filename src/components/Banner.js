502f4c76527dbbfcb9cfbb2af203ba2ce0c2f0d5
dankasolution <dankasolution@gmail.com>
2020-02-20 14:06:30 +1100

    "tidy up styles"

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,20 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import { CarouselItemOne } from './CarouselItemOne';
 import { CarouselItemTwo } from './CarouselItemTwo';
 
 export const Banner = () =>{
-    const [index, setIndex] = useState(0);
-    const [direction, setDirection] = useState(null);
-  
-    const handleSelect = (selectedIndex, e) => {
-        console.log(e, selectedIndex);
-        setIndex(selectedIndex);
-        setDirection(e.direction);
-    };
-
     return(
-        <Carousel interval={null} controls={false} direction={direction} onSelect={handleSelect}>
+        <Carousel interval={null} controls={false}>
             <Carousel.Item className="carouselItemOne">
                 <CarouselItemOne />
             </Carousel.Item>
